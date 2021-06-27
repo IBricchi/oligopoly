d12d0d01a773d6791a80cdfd61a723f64814ec70
IBricchi <ignacio.bricchi@gmail.com>
2021-06-27 15:20:38 +0100

    prevent rolling action while action is already occuring

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -10,6 +10,9 @@ var player: KinematicBody
 
 var dice: RigidBody
 
+var global_time: int = 0
+var memory: Dictionary = {}
+
 signal player_step(target)
 signal request_board_tiles(tiles)
 signal roll_dice
@@ -20,18 +23,27 @@ func _ready():
 	
 	dice = $dice
 	dice.connect("rolled_value", self, "_on_rolled_value")
+	dice.connect("player_landed", self, "_on_player_landed")
 
+var can_roll: bool = true
 func _process(delta):
 	if !tiles_set:
 		tiles_set = false
 		emit_signal("request_board_tiles", board_tiles)
 	
-	if Input.is_action_just_pressed("lc"):
+	if can_roll and Input.is_action_just_pressed("lc"):
+		can_roll = false
 		emit_signal("roll_dice")
 
 func _on_rolled_value(val: int):
+	can_roll = true
 	var target: int = (current_tile + val)%board_tiles.size()
 	while current_tile != target:
 		current_tile = (current_tile + 1) % board_tiles.size()
 		emit_signal("player_step", board_tiles[current_tile])
-	
+
+func _on_player_landed(player_time: int):
+	memory[global_time] = {
+		"player_time": player_time,
+		"position": current_tile
+	}
