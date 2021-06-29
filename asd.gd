d28d4721d5d09710cca239cac6453323708a5792
Vincent-Sietses <72189806+Vincent-Sietses@users.noreply.github.com>
2021-06-30 00:21:07 +0200

    Added stuff to asd.gd (test)
    Added tile types, will flesh out each tile type soon.
    Created functions player_lands() and player_passes() which will be called from game.gd

--- asd.gd
+++ asd.gd
@@ -1,5 +1,25 @@
 extends Spatial
 
+export var tile_type : int = 0
+# 0 is a property tile
+# 1 is a chance card
+# 2 is a time travel card
+# 3 is start
+
+
+var owners: Dictionary = {}
+# dictionry which stores owners and the turn where they initially were bought
 
 func _ready():
 	pass
+	
+	
+func player_lands():
+	if tile_type == 0:
+		if owners.empty():
+			pass # subtract some money from player
+
+
+func player_passes():
+	if tile_type == 3:
+		pass # add money to player
